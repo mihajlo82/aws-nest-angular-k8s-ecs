9238cb6247d9ba24f522b603712428f66b8442f9
Mihajlo <dimitricmihajlo995@gmail.com>
2026-02-17 14:01:06 +0100

    changes

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { HealthController } from './health/health.controller';
 
 @Module({
   imports: [TasksModule],
-  controllers: [AppController],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
